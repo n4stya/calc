e75188359575dfa66ca62dc9c1bb07463137939e
n4stya <marmysh111@gmail.com>
2024-07-24 14:07:42 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,83 @@
+let a = ''; //первое число
+let b = ''; // второе число
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+const out = document.querySelector('.calc-screen p')
+
+function clearAll() {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = '0';
+}
+
+document.querySelector('.c').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    if (!event.target.classList.contains('btn')) return;
+    if (event.target.classList.contains('c')) return;
+
+    out.textContent = '';
+    const key = event.target.textContent;
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+            console.log(a, b, sign);
+            out.textContent = a;
+        }
+        else if (a !== '' && b !== '' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b, sign);
+        return;
+    }
+
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b, sign);
+        return;
+    }
+
+    if (key === '=') {
+        if (b === '') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "X":
+                a = a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b, sign);
+    }
+
+}
+
+
